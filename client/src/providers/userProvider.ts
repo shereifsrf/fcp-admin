fa9458d651a2c4d5c5b4abb43c2dbcaab62b945b
Shereif <chachushereef@gmail.com>
2021-08-01 05:04:56 +0800

    add production urls

--- client/src/providers/userProvider.ts
+++ client/src/providers/userProvider.ts
@@ -3,7 +3,10 @@ import { stringify } from "querystring";
 import { fetchUtils } from "react-admin";
 import { getLocalStorage } from "../utils";
 
-const apiUrl = "http://localhost:6006/v1";
+const mode = process.env.NODE_ENV;
+const serverUrl = process.env.SERVER_URL;
+const localUrl = process.env.LOCAL_URL;
+let apiUrl = mode === "production" ? serverUrl : localUrl;
 
 const fetchJson = (url: string, options: any = {}) => {
     if (!options.headers) {
